4828e7b30ab2a0db13291480572d0a95823adff2
Patrik Ekenberg <Patrik@Ekenberg.com>
2019-11-15 15:56:03 +0000

    Completed initialization and added a few tests

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3.7 
 # SciPy 1.3.1 doesn't seem to be compatiable with Python 3.8. Thus, I will use Python 3.7
-RUN pip install --no-cache-dir torch gym numpy matplotlib pandas
+RUN pip install --no-cache-dir torch gym numpy matplotlib pandas pytest
 CMD ["cat", "/etc/os-release"]
--- game/game.py
+++ game/game.py
@@ -15,7 +15,7 @@ class azul:
         self.floors=np.zeros((players,7),dtype=bool)
         self.score=np.zeros(players,dtype=int)
         self.firstPlayer=1
-    def newRound(self):
+    def new_round(self):
         #Game board center starts empty except for the first player token (sixth index).
         self.gameBoardCenter=[0,0,0,0,0,1]
         #Factory displays have 4 random tiles (TODO: respect total number of tiles)
@@ -26,5 +26,42 @@ class azul:
 
 if __name__ == "__main__":
     game=azul()
-    game.newRound()
+    game.new_round()
     print(str(game.gameBoardDisplays))
+
+# TESTS Move these later
+
+def test_azulInit():
+    game=azul()
+    game4Players=azul(players=4)
+    # gameBoardDisplays should be of size (5,5).
+    # TODO: Size should change depending on nr of players
+    assert game.gameBoardDisplays.shape == (5,5)
+    # gameBoardCenter should be of size 6
+    assert game.gameBoardCenter.shape == (6,)
+    # patternLines should be of size (nrOfPlayers,5,5)
+    assert game.patternLines.shape == (2,5,5)
+    assert np.count_nonzero(game4Players.patternLines) == 0
+    assert game4Players.patternLines.shape == (4,5,5)
+    # walls should be of size (nrOfPlayers,5,5) and be empty
+    assert game.walls.shape == (2,5,5)
+    assert np.count_nonzero(game4Players.walls) == 0
+    assert game4Players.walls.shape == (4,5,5)
+    # floor should be of size (nrOfPlayers,7)
+    assert game.floors.shape == (2,7)
+    assert game4Players.floors.shape == (4,7)
+    # score should be of size (nrOfPlayers,) and be zero
+    assert game.score.shape == (2,)
+    assert np.count_nonzero(game4Players.score) == 0
+    assert game4Players.score.shape == (4,)
+
+def test_azul_new_round():
+    game=azul()
+    game.new_round()
+    #After new round, no display should have more than 4 tiles
+    for tile in game.gameBoardDisplays:
+        assert np.sum(tile) == 4
+    #After new round, game board center should have zero color tiles
+    assert np.count_nonzero(game.gameBoardCenter[:5]) == 0
+    #After new round, game board center should have one white tile
+    assert game.gameBoardCenter[5] == 1
